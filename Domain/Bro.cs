dc02caf761c94f311179a5645b915f790805c88b
JensMalm <jensmalm873@gmail.com>
2018-12-31 12:10:54 +0100

    Ef Packages added

--- Domain/Bro.cs
+++ Domain/Bro.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain
 {
     public class Bro
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
-        public Week Weeks { get; set; }
-        public double Points { get; set; }
+        public List<Week> Weeks { get; set; }
     }
 }
---
+++ WebApp/Controllers/TippController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class TippController : Controller
+    {
+        public IActionResult AddBro(string name)
+        {
+            //var bro=new Bro
+            //{
+               
+            //}
+            //using (var context = new TippContext())
+            //{
+            //    context.Add()
+            //}
+            return View();
+        }
+    }
+}
